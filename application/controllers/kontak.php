5902e6cdd7b1163547260eafb7ecf2a91b1f69bd
reza2212 <rezaprasetyo77@gmail.com>
2020-03-07 02:36:57 +0700

    Reza Dwi Prasetyo (E41182197) RestServer

---
+++ application/controllers/kontak.php
@@ -0,0 +1,66 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . '/libraries/REST_Controller.php';
+use Restserver\Libraries\REST_Controller;
+
+class Kontak extends REST_Controller {
+
+    function __construct($config = 'rest') {
+        parent::__construct($config);
+        $this->load->database();
+    }
+    //untuk membaca table
+    function index_get() {
+        $id = $this->get('id');//membaca otomatis data yang ada
+        if ($id == '') {
+            $kontak = $this->db->get('telepon')->result();//menampilkan semua data yang ada didatabase
+        } else {
+            $this->db->where('id', $id);
+            $kontak = $this->db->get('telepon')->result();
+        }
+        $this->response($kontak, 200);
+    }
+    //untuk mengirim data baru
+    function index_post() {
+        $data = array(
+                    'id'           => $this->post('id'),
+                    'nama'          => $this->post('nama'),
+                    'nomor'    => $this->post('nomor'));
+        $insert = $this->db->insert('telepon', $data);//memasukkan data
+        if ($insert) {
+            $this->response($data, 200);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+    //untuk memperbarui data yang telah ada
+    function index_put() {
+        $id = $this->put('id');//memperbarui data otomatis berdasarkan id yang dipilih
+        $data = array(
+                    'id'       => $this->put('id'),
+                    'nama'          => $this->put('nama'),
+                    'nomor'    => $this->put('nomor'));
+        $this->db->where('id', $id);
+        $update = $this->db->update('telepon', $data);
+        if ($update) {
+            $this->response($data, 200);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+    //untuk menghapus data
+    function index_delete() {
+        $id = $this->delete('id');
+        $this->db->where('id', $id); //menghapus otomatis berdasarkan id yang dipilih
+        $delete = $this->db->delete('telepon');
+        if ($delete) {
+            $this->response(array('status' => 'success'), 201);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+
+}
+?>
